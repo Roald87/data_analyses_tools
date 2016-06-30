1c460eddbec612c27def4cd33c716e8062d7ba16
Roald87 <roaldruiter@gmail.com>
2016-06-30 21:24:22 +0200

    Changed from setuptools to distutils.core

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(
-    name='data_analyses_tools',
+setup(name='data_analyses_tools',
     version='0.1.0',
     description='Functions for data analyses and processing.',
     long_description=open('README.md').read(),
     author='Roald Ruiter',
     author_email='roaldruiter@gmail.com',
     url='https://github.com/Roald87/data_analyses_tools',
+    py_modules=['data_analyses_tools'],
 )
